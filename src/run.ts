eb08a9da5024a6fa75ccd68946c7a3738588e670
Alex Dev Sm <leshasmlesha@gmail.com>
2022-08-25 14:05:41 +0300

    Добавил прогресс

--- src/run.ts
+++ src/run.ts
@@ -1,6 +1,7 @@
 import getConfig from './utils/config';
 import { MangaClient } from './clients/manga-client';
 import { AdapterBase } from './base/adapter';
+import cliProgess from 'cli-progress';
 
 async function start(adapter: AdapterBase) {
   await adapter.required();
@@ -13,7 +14,14 @@ async function start(adapter: AdapterBase) {
     const manga_online = await client.search(mangaLocal.title);
     const chapters = mangaLocal.listChapter();
     console.log(`Загрузка глав в кол-ве ${chapters.length}`);
+    const bar = new cliProgess.SingleBar({
+      format: `Загрузка манги ${mangaLocal.title} | {bar} | {percentage}%| {value}/{total} Глав | Текущая глава: {volume}`,
+    });
+    bar.start(chapters.length, 0);
     for (const chapter of chapters) {
+      bar.increment({
+        volume: `Том ${chapter.volume} глава ${chapter.chapter} - ${chapter.title}`,
+      });
       await manga_online.upload(
         chapter.volume,
         chapter.chapter,
@@ -21,11 +29,14 @@ async function start(adapter: AdapterBase) {
         chapter.title,
       );
     }
+    bar.stop();
   }
+  console.log('Загрузка завершена');
 }
 export async function call(adapter: AdapterBase) {
   try {
     await start(adapter);
+    process.exit();
   } catch (e) {
     if (e instanceof Error) {
       console.log(e.message);
