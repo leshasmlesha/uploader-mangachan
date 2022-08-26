d01de54818a1d84a9244048627e09e5717378f9b
Alex Dev Sm <leshasmlesha@gmail.com>
2022-08-26 13:35:13 +0300

    Ожидание при настройке

--- src/main.ts
+++ src/main.ts
@@ -3,17 +3,24 @@ import { AdapterBase } from './base/adapter';
 import { parseArgumentsIntoOptions, promptForMissingOptions } from './cli';
 import { call } from './run';
 import getConfig from './utils/config';
+import wait_input from './utils/wait_input';
 export async function cli(args: string[]) {
-  const config = getConfig();
-  let options = parseArgumentsIntoOptions(args);
-  if (options.interactive) options = await promptForMissingOptions(options);
-  const adapterStatic = adapters.find(
-    (adapter) => adapter.adapter === options.adapter,
-  );
-  const adapter: AdapterBase = new adapterStatic(
-    options.work,
-    options['search-id'],
-  );
-  call(adapter, config, options);
+  try {
+    const config = getConfig();
+    let options = parseArgumentsIntoOptions(args);
+    if (options.interactive) options = await promptForMissingOptions(options);
+    const adapterStatic = adapters.find(
+      (adapter) => adapter.adapter === options.adapter,
+    );
+    const adapter: AdapterBase = new adapterStatic(
+      options.work,
+      options['search-id'],
+    );
+    await call(adapter, config, options);
+  } catch (e) {
+    console.log(e.message);
+    await wait_input('Нажмите Enter чтобы выйти');
+    process.exit();
+  }
 }
 cli(process.argv);
