b87f8f8f914551ea2f3e896075695fc26cad138e
Alex Dev Sm <leshasmlesha@gmail.com>
2022-08-26 11:58:22 +0300

    Проверка авторизации и поиска манги

--- src/clients/manga-client.ts
+++ src/clients/manga-client.ts
@@ -7,12 +7,19 @@ export class MangaClient {
     this.client = new HttpClient(url);
   }
   async login(login: string, password: string) {
-    await this.client.postCookies('/', {
+    const data = await this.client.postCookies('/', {
       login: 'submit',
       login_name: login,
       login_password: password,
       image: 'Вход',
     });
+    const top_user = data.window.document.querySelector(
+      '#top_user',
+    ) as HTMLDivElement;
+    const form = top_user.querySelector('form');
+    if (form) {
+      throw new Error('Вы не прошли авторизацию');
+    }
   }
   async search(value: string, id?: boolean, demo?: boolean) {
     if (demo) return null;
@@ -27,6 +34,7 @@ export class MangaClient {
       const row = data.window.document.querySelector(
         `.content_row`,
       ) as HTMLDivElement;
+      if (!row) throw new Error('Манга не найдена');
       const link = row.querySelector('h2 > a') as HTMLLinkElement;
       return new Manga(this, link.href);
     }
